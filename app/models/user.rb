54d07bb927f09d7dd5c4b573a23c817a90d571b4
barchuk oleg <barchukoleg12@gmail.com>
2020-11-22 18:27:46 +0200

    add user to events + validations for event and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  EMAIL_FORMAT = /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
+
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    uniqueness: true,
+                    format: EMAIL_FORMAT
 end
