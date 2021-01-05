a3be9f8e01ee117abd60d4369e9baae19cea122a
barchuk oleg <barchukoleg12@gmail.com>
2021-01-05 17:44:23 +0200

    refactor with internationalization

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module EventOrganizer
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.default_locale = :uk
+    config.i18n.default_locale = :en
 
     # Don't generate system test files.
     config.generators.system_tests = nil
