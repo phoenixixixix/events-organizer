d680a5495ba991f168af211ba90b01bff1013611
barchuk oleg <barchukoleg12@gmail.com>
2021-01-09 22:44:43 +0200

    many to many setting 1.2

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,10 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
 
+  # Event has many subscribers(User objects), through
+  # subscriptions table, by user_id key
+  has_many :subscribers, through: :subscriptions, source: :user
+
   validates :user, presence: true
   validates :title, presence: true,
                     length: { maximum: 255 }
