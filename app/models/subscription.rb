0fc0f627aa4a18d336f370955a1a9a6e30a6c44c
barchuk oleg <barchukoleg12@gmail.com>
2021-01-09 22:11:44 +0200

    add scope for validation uniqueness registered and unregistered users

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,6 +10,9 @@ class Subscription < ApplicationRecord
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: { with: EMAIL_FORMAT }, unless: -> { user.present? }
 
+  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+
   def user_name
     if user.present?
       user.name
