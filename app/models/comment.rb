d6eefb24ac3c0a4f91934ae0c96bd1e201ade8bb
barchuk oleg <barchukoleg12@gmail.com>
2021-01-09 16:25:47 +0200

    setting dependencies + code style corrected

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  #optional: true, means now can create comment without user
+  belongs_to :user, optional: true
 
-  validates :event, presence: true
   validates :body, presence: true
-  validates :user_name, presence: true, unless: 'user.present?'
+  validates :user_name, presence: true, unless: -> { user.present? }
 
   def user_name
     if user.present?
