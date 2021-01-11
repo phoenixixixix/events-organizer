211489435e2fb18a7b74319db5f4dfd8dcb70f26
barchuk oleg <barchukoleg12@gmail.com>
2021-01-11 15:50:01 +0200

    add fa icons, previously added icon tags now work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
   def user_avatar(user)
     # TODO: user real avatars
-
     asset_path('user.png')
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
 end
