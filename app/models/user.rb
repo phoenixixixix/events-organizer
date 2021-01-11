0568c216675bd41a50f36ce5ad066822bc301803
barchuk oleg <barchukoleg12@gmail.com>
2021-01-11 18:08:48 +0200

    link_subscriptions method for created User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,17 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  # checks newly created user for subscriptions(by email)
+  # and update if any exist
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = "User №#{rand 1000}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
   end
 end
